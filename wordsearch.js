d56eec4781bdc2285eb70c4884a2c9203e254fe0
Smiti <mishra.smiti06@gmail.com>
2021-09-22 20:24:09 -0400

    fixed error

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,46 @@
+const transpose = function(matrix) {
+
+    const nRows = matrix[0].length;
+    const nCols = matrix.length;
+  
+    let transposedArray = [];
+    
+    for (let i = 0; i < nRows; i++) {
+      let tempArray = Array(nCols).fill(0);
+      for (let j = 0; j < nCols; j++) {
+        let temp = matrix[j][i];
+        tempArray[j] = temp;
+      }
+      transposedArray.push(tempArray);
+    }
+    return transposedArray;
+  };
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word))
+        { 
+            return true
+        } 
     }
+
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word))
+        { 
+            return true
+        } 
+    } 
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
+
+
+
+
+
+
+ 
